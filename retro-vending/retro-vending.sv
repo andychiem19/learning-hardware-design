c5925a6e668ed4a91e69180d4727d6b7538e56c8
andychiem19 <ac4626@drexel.edu>
2025-06-25 19:15:46 -0400

    Update retro-vending.sv

--- retro-vending/retro-vending.sv
+++ retro-vending/retro-vending.sv
@@ -60,27 +60,3 @@ module retro_vending (
       	dispense <= 0;
   end
 endmodule
-
-module coin_accumulator (
-  input clk,
-  input reset,
-  input clear,
-  input coin_5,
-  input coin_10,
-  input coin_25,
-  output reg [6:0] total
-);
-  
-  always @(posedge clk or posedge reset) begin
-    if (reset || clear)
-      total <= 0;
-    else if (coin_5)
-      total <= total + 5;
-    else if (coin_10)
-      total <= total + 10;
-    else if (coin_25)
-      total <= total + 25;
-  end
-endmodule
- 
-    
